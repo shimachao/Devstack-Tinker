4fc83d61dc3cc53f3ec5bf33b0ce026c84b6f281
chao <531236305@qq.com>
2016-09-25 14:04:13 +0800

    Rename pip.conf

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
-sudo sh -c 'cat ./sources.list > /etc/apt/sources.list'
+sudo sh -c 'cat ./xidian-14.04-sources.list > /etc/apt/sources.list'
 sudo apt-get update
 
 mkdir ~/.pip
-cp ./pip.conf ~/.pip/pip.conf
+cp ./xidian-pip.conf ~/.pip/pip.conf
 sudo mkdir /root/.pip
-sudo cp ~/pip.conf /root/.pip/pip.conf
+sudo cp ./xidian-pip.conf /root/.pip/pip.conf
 
 sudo apt-get -y install python-pip python3-pip python-setuptools python3-setuptools git vim openssh-server fabric
 
